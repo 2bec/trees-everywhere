d627c5c82a603240c76dd7df5115c1d365b6ddc0
Carlos E Berton <berton.5b@gmail.com>
2022-07-18 15:16:07 -0300

    improvements: add user app

---
+++ users/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import Profile, User
+
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Profile)
