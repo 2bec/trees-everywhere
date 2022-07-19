38a0c67e08570d1e757c02594e3c1cbbd173c102
Berton, Carlos <berton.5b@gmail.com>
2022-07-19 12:06:57 -0300

    [Test] User methods (#9)
    * tests: user methods and change trees model and fixtures
    
    * fix: template layout and poredirect root path to login

--- trees/models.py
+++ trees/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 from accounts.models import Account
 from trees.managers import PlantedTreesManager
-from users.models import User
 
 
 class Tree(models.Model):
@@ -16,11 +15,18 @@ class Tree(models.Model):
 class PlantedTree(models.Model):
     age = models.IntegerField(default=0)
     planted_at = models.DateTimeField("date tree was planted", auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
     tree = models.ForeignKey(Tree, on_delete=models.CASCADE)
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
 
     objects = PlantedTreesManager()
 
     def __str__(self):
         return self.tree
+
+    @property
+    def location(self):
+        return (self.latitude, self.longitude)
--- trees_everywhere/urls.py
+++ trees_everywhere/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url=reverse_lazy("login"), permanent=False)),
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("trees/", include("trees.urls")),
